e5f55a52364d7b76f53abd049c30bf83373ad20f
서재원 <cinos81@gmail.com>
2020-12-30 15:16:46 +0900

    코멘트 추가

--- craco.config.js
+++ craco.config.js
@@ -3,7 +3,8 @@ module.exports = {
 		configure: (webpackConfig, {env, paths}) => {
 			const newRules = Object.assign({}, webpackConfig.module.rules.find(rule => rule.oneOf))
 			const babelRule = newRules.oneOf.find(rule => rule.loader.includes('babel-loader'))
-			// 컴파일할 라이브러리를 추가하도록 exclude 프로퍼티에 정규표현식을 추가
+			// node_modules 하위의 라이브러리 중에 es5를 지원하지 않는 부류를 필터한다
+			// babel은 그것을 다시 한번 컴파일하게 된다.
 			babelRule.exclude = /[\\/]node_modules[\\/](?!(@testing-library\/react|@testing-library\/user-event)[\\/])/
 			const otherRules = webpackConfig.module.rules.filter(rule => !rule.oneOf)
 
